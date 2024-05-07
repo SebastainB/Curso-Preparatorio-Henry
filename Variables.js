939e33c43a6ae9797b45e08bec333d926cdf603f
Jano <sbeisso@icloud.com>
2024-05-07 18:34:37 -0300

    first commit

---
+++ Variables.js
@@ -0,0 +1,27 @@
+//String
+
+var bootcamp = 'Henry';
+
+//Numeros
+
+var numeroEntero = 17;
+
+var numeroNegativo = -5;
+
+var numeroDecimal = 12.50;
+
+//Booleanos
+
+var estoyEntendiendo = true;
+
+var estoyAburrido = false;
+
+//Undefined
+
+var cajon1;
+
+console.log(cajon1);
+
+//Null
+
+var sinNada = null; 
---
+++ operadores.js
@@ -0,0 +1,4 @@
+console.log("Hola " + "Santi");
+var nombre = "Camilo";
+var saludo = "Hola " + nombre;
+console.log(saludo);
